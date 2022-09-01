2eea9567730beccd730a4c309c41bf1a7a7cb246
Maroccostar <maroccostar@i.ua>
2022-09-01 18:22:41 +0300

    /

--- test1/FilmTableViewCell.swift
+++ test1/FilmTableViewCell.swift
@@ -1,4 +1,4 @@
-
+ 
 import UIKit
 
 class FilmTableViewCell: UITableViewCell {
@@ -28,4 +28,4 @@ class FilmTableViewCell: UITableViewCell {
         super.layoutSubviews()
         titleLabel.pin.horizontally(16).vertically()
     }
-} 
+}
